a9afec2387ae66a7cd4019aafa1d32806435a4c2
Akanksha Agrawal <kumarianshuagrawal847@gmail.com>
2024-06-02 22:48:19 +0530

    Updates

---
+++ precache-manifest.5475985fb1f0c9dc92b21315b12bb763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd814cef1b4c4845e7c1558ed6273bc",
+    "url": "/Amazon_clone/index.html"
+  },
+  {
+    "revision": "cd6f1937ee0bb34feeb4",
+    "url": "/Amazon_clone/static/css/main.0d9e319a.chunk.css"
+  },
+  {
+    "revision": "d16e7cce37a03b0d2441",
+    "url": "/Amazon_clone/static/js/2.9ab8196f.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/Amazon_clone/static/js/2.9ab8196f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6f1937ee0bb34feeb4",
+    "url": "/Amazon_clone/static/js/main.681f2d9d.chunk.js"
+  },
+  {
+    "revision": "2bd20bdbe79a7972b0f8",
+    "url": "/Amazon_clone/static/js/runtime-main.2aea997b.js"
+  }
+]);
